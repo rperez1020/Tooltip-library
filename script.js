d6ddd44a1de91bcbd8cd6490edd9df7896867c99
rperez1020 <49043860+rperez1020@users.noreply.github.com>
2023-12-18 20:38:16 -0800

    update

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-const top = document.getElementById("top")
-
-document.onkeydown = insertText
-
-function insertText(e){
-    e = e || window.event
-    if(e.keyCode == "38"){
-        top.innerText = "tool tip"
+document.addEventListener("keydown", e => {
+    const container = document.querySelector(".container")
+  
+    switch (e.key) {
+      case "ArrowLeft":
+        container.classList.replace("x-center", "x-left")
+        container.classList.replace("x-right", "x-center")
+        break
+      case "ArrowRight":
+        container.classList.replace("x-center", "x-right")
+        container.classList.replace("x-left", "x-center")
+        break
+      case "ArrowUp":
+        container.classList.replace("y-center", "y-top")
+        container.classList.replace("y-bottom", "y-center")
+        break
+      case "ArrowDown":
+        container.classList.replace("y-center", "y-bottom")
+        container.classList.replace("y-top", "y-center")
+        break
     }
-}
+  })
